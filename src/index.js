4aca771b9aa3ab9e6c5f1f1e59c00aeaa8c4934e
Ken Bigler <ken_bigler@intuit.com>
2022-01-26 13:44:28 -0800

    feat: update deployement url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://odyssey-gql-track-tutorial.herokuapp.com/',
+  uri: 'https://gql-track-tutorial-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
